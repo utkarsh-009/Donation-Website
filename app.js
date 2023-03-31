f8aff935e5fe006bd61950f2edc13d6c54612636
utkarsh-009 <utkarshpatil321@gmail.com>
2023-03-31 16:15:48 +0530

    pug templates added

--- app.js
+++ app.js
@@ -16,7 +16,12 @@ app.set('views', path.join(__dirname, 'views')); // Sets the views directory
 // GET REQUEST
 app.get('/', (req, res)=>{
     const params = {}
-    res.status(200).render('index.pug', params);
+    res.status(200).render('home.pug', params);
+})
+
+app.get('/contact', (req, res)=>{
+    const params = {}
+    res.status(200).render('contact.pug', params);
 })
 
 // START SERVER
