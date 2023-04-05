bd6bf545f0bfb5bebcd0d3bcca80f08707a54452
utkarsh-009 <utkarshpatil321@gmail.com>
2023-04-05 23:20:57 +0530

    final changes

--- static/index.js
+++
@@ -1,6 +0,0 @@
-// Establishing connection to db
-const mongoose = require('mongoose');
-main().catch(err => console.log(err));
-async function main() {await mongoose.connect('mongodb://127.0.0.1:27017/ekart');}
-
-
